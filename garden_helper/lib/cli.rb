124748070632b4a2102aac388fd08b83fcfe4461
David Markel <davidgmarkel@gmail.com>
2018-04-17 17:34:08 +0000

    Reformatted text for readability in cli.rb

--- garden_helper/lib/cli.rb
+++ garden_helper/lib/cli.rb
@@ -2,15 +2,17 @@ require 'date'
 require 'pry'
 
 class GardenHelper::CLI
-attr_accessor :location
+  attr_accessor :location
 
   def call
+    puts ""
     puts "Garden Helper helps you determine which vegetables you should be planting in your garden this month."
     puts "To best help you, we need to know what growing zone you live in."
     puts ""
     puts "Let's get started!"
     puts "Please enter your city or state below."
     real_location?
+    puts ""
     puts "Hiya, #{@location}!  We've got your growing zone down."
     puts "For the month of #{Date::MONTHNAMES[Date.today.month]}, we're recommending that you plant the following vegetables in your area."
     list_veg_by_location#(location)
@@ -28,10 +30,12 @@ attr_accessor :location
 
     def list_veg_by_location#(location)
       #veg_of_the_month_by_location(location)
+      puts ""
       puts "Tomatoes"
       puts "Zucchini"
       puts "Eggplant"
       puts "Peppers"
+      puts ""
       puts "For more growing information, enter any of the vegetables listed above."
       puts "You can also exit at any time by typing the magic word (It's exit)."
       user_input
