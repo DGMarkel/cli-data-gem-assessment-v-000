36e845a4696c67f50ec85f8cde9ed54aaa3fa5fc
David Markel <davidgmarkel@gmail.com>
2018-04-17 15:39:15 +0000

    Added CLI to ./lib

---
+++ garden_helper/lib/cli.rb
@@ -0,0 +1,7 @@
+class GardenHelper::CLI
+
+  def call
+    puts "Hello world"
+  end
+
+end
--- garden_helper/lib/garden_helper.rb
+++ garden_helper/lib/garden_helper.rb
@@ -1,4 +1,5 @@
 require "garden_helper/version"
+require 'lib'
 
 module GardenHelper
   # Your code goes here...
