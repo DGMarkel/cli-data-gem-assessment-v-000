652d5728c6a79807341c263023c7772de84bf54a
David Markel <davidgmarkel@gmail.com>
2018-04-17 16:24:57 +0000

    Added #call

--- garden_helper/lib/cli.rb
+++ garden_helper/lib/cli.rb
@@ -1,7 +1,21 @@
 class GardenHelper::CLI
 
   def call
-    puts "Hello world"
+    puts "Garden Helper helps you determine which vegetables you should be planting in your garden this month."
+    puts "To best help you, we need to know what growing zone you live in."
+    puts "Please enter your city or state below."
+
+    location = gets.strip
+    if location.to_i == 0
+      input.downcase.capitalize
+    else
+      puts "That's not a real place.  Please try again."
+    end
+
+    #for now, location is just Brooklyn
+    location
+    list_veg_by_location(location)
   end
+
 
 end
