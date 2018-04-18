84a69c55fa8f94bb22658f48a73f963eca0c432e
David Markel <davidgmarkel@gmail.com>
2018-04-18 04:02:43 +0000

    Success scraping growing dates from garden.org. On to greater things

--- garden_helper/lib/scraper.rb
+++ garden_helper/lib/scraper.rb
@@ -6,11 +6,14 @@ class GardenHelper::Scraper
   def self.scrape_index_page(location)
     a = []
 
-    doc = Nokogiri::HTML(open("https://garden.org/apps/calendar/?q=Brooklyn"))
+    doc = Nokogiri::HTML(open("https://garden.org/apps/calendar/?q=" + "#{location}"))
     crops = doc.css("tr")
 
     crops.each do |crop|
       new_vegetable = GardenHelper::Vegetable.new("#{crop.css("td[data-th='Crop']").text.gsub("\n", "")}")
+      new_vegetable.sow_seeds_indoors = "#{crop.css("td[data-th='Sow seeds indoors']").text.gsub("\n", "")}"
+      new_vegetable.transplant_seedlings = "#{crop.css("td[data-th='Transplant seedlings into the garden']").text.gsub("\n", "")}"
+      new_vegetable.direct_sow_seeds = "#{crop.css("td[data-th='Direct sow seeds']").text.gsub("\n", "")}"
       a << new_vegetable if new_vegetable.name != ""
     end
     a
--- garden_helper/lib/vegetable.rb
+++ garden_helper/lib/vegetable.rb
@@ -1,23 +1,9 @@
 class GardenHelper::Vegetable
-  attr_accessor :name, :scientific_name, :description, :popular_varieties, :growing_conditions, :url
+  attr_accessor :name, :sow_seeds_indoors, :transplant_seedlings, :direct_sow_seeds
   @@vegetables = []
 
   def initialize(name)
     @name = name
   end
-
-  def self.month
-    #I should return a bunch of instances of vegetables for this month
-    tomato = self.new
-    tomato.name = "Tomatoes"
-    tomato.scientific_name = "Tomatus Tomaticum"
-    tomato.description = "Tomatoes come in many colors, and all of them are equally delicious"
-    tomato.popular_varieties = "Bling, blong, and blue."
-    tomato.growing_conditions = "Sandy Soil, Ample Sunshine"
-    tomato.url = "http://www.wikipedia.com/#{tomato.name}"
-    tomato
-    puts "#{tomato.name}"
-  end
-
 
 end
