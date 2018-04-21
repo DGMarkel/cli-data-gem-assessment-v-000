4a482d8950bd9b9ff72d0d8997981023f5682c6c
David Markel <davidgmarkel@gmail.com>
2018-04-21 18:04:03 +0000

    automatically backed up by learn

--- garden_helper/lib/cli.rb
+++ garden_helper/lib/cli.rb
@@ -60,6 +60,7 @@ class GardenHelper::CLI
   end
 
   def formatted_description(vegetable)
+    puts ""
     vegetable.description.scan(/\b.*\b/).each do |sentence|
       print sentence + ". " if sentence.length > 15
       if sentence.length < 15 && sentence != ""
